35d3c3651ec21d01005b6f8b32e9bdf953bd38a4
vector76 <vector76@gmail.com>
2022-04-26 22:04:30 -0500

    Rendered STLs from earboxer

--- _render_all.bat
+++ _render_all.bat
@@ -22,4 +22,22 @@
 %OPENSCAD% gridfinity_flsun_q5.scad -o flsun_q5.stl --export-format binstl
 %PYTHON% canonicalize.py flsun_q5.stl
 
+%OPENSCAD% gridfinity_socket_holder.scad -D part=1 -o socket_holder_metric.stl --export-format binstl
+%PYTHON% canonicalize.py metric.stl
+
+%OPENSCAD% gridfinity_socket_holder.scad -D part=2 -o socket_holder_imperial.stl --export-format binstl
+%PYTHON% canonicalize.py socket_holder_imperial.stl
+
+%OPENSCAD% gridfinity_socket_holder.scad -D part=3 -o socket_holder_imperial_small.stl --export-format binstl
+%PYTHON% canonicalize.py socket_holder_imperial_small.stl
+
+%OPENSCAD% gridfinity_socket_holder.scad -D part=4 -o socket_holder_imperial_big.stl --export-format binstl
+%PYTHON% canonicalize.py socket_holder_imperial_big.stl
+
+%OPENSCAD% gridfinity_socket_holder.scad -D part=5 -o socket_holder_metric_stacking.stl --export-format binstl
+%PYTHON% canonicalize.py socket_holder_metric_stacking.stl
+
+%OPENSCAD% gridfinity_socket_holder.scad -D part=6 -o socket_holder_metric_small.stl --export-format binstl
+%PYTHON% canonicalize.py socket_holder_metric_small.stl
+
 pause
